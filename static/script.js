ae139b2695dded5a0874c295d3d068a87433a9a6
Sam Fuchs <samcfuchs@gmail.com>
2022-02-08 22:25:30 -0500

    Build scatterplot viz

--- static/script.js
+++ static/script.js
@@ -1,12 +1,13 @@
 //console.log("Hello!")
 
-const MARGIN = {top: 10, bottom: 10, left: 10, right: 10}
+const MARGIN = {top: 10, bottom: 20, left: 40, right: 10}
 var svg = d3.select("svg#viz")
 const width = svg.attr('width')
 const height = svg.attr('height')
 const m_width = width - (MARGIN.left + MARGIN.right)
 const m_height = height - (MARGIN.top + MARGIN.bottom)
-const map = svg.append('g').attr("transform",`translate(${MARGIN.left}, ${MARGIN.top})`);
+//const map = svg.append('g').attr("transform",`translate(${MARGIN.left}, ${MARGIN.top})`);
+const chart = svg.append('g').attr("transform",`translate(${MARGIN.left}, ${MARGIN.top})`);
 
 console.log(svg)
 
@@ -14,10 +15,51 @@ const requestData = async function() {
     const sf = await d3.csv("Street_Tree_List-2022-01-30_FILTERED.csv")
     const context = await d3.json("SF-Neighborhoods.geo.json")
     const neighborhoods = topojson.feature(context, context.objects.SFNeighborhoods)
-    
+
+    const datefmt = "%m/%d/%y %H:%M"
+    var dateparser = d3.timeParse(datefmt)
+
+    // parse dates
+    let n = 0
+    sf.forEach(d => {
+        let date = dateparser(d.PlantDate);
+
+        if (date != null) { 
+            let year = date.getFullYear()
+            if (year > 2022) { date.setFullYear(year - 100) }
+            d.date = date
+        } else { d.date = new Date(1965, 0, 1); n++; }
+
+    })
 
     console.log(sf)
+    console.log("Number of missing dates: " + n)
 
+    console.log(dateparser)
+    var x_scale = d3.scaleTime()
+        .domain(d3.extent(sf, d => d.date))
+        .range([width - (MARGIN.left + MARGIN.right), 0])
+    //var y_scale = d3.scaleLog()
+    var y_scale = d3.scaleLinear()
+        .domain(d3.extent(sf, d => d.DBH))
+        .range([width - (MARGIN.left + MARGIN.right), 0])
+    
+    chart.selectAll("circle.tree").data(sf)
+        .join("circle")
+        .attr("class", "tree")
+        .attr("fill", "#000")
+        .attr("opacity", 0.2)
+        .attr("r", 2)
+        .attr('cx', d => x_scale(d.date))
+        .attr('cy', d => y_scale(d.DBH));
+
+    x_axis = d3.axisBottom().scale(x_scale)
+    y_axis = d3.axisLeft().scale(y_scale)
+
+    let axes = chart.append("g").attr("class", 'axes');
+        axes.append('g').call(x_axis).attr('transform',`translate(0,${m_height})`)
+        axes.append('g').call(y_axis)
+    /*
     // Map stuff
     var projection = d3.geoMercator().fitSize([m_width, m_height], neighborhoods)
     var path = d3.geoPath().projection(projection)
@@ -48,7 +90,7 @@ const requestData = async function() {
     
     d3.axisLeft(y_scale)
     d3.axisBottom(x_scale)
-
+    */
     /*
     map.selectAll("circle.point").data(sf)
         .join("circle")
